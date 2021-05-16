d3f2abf807830d1fed8a2dbd760b004e23efa7ce
VictorPlesko <victorplesko@gmail.com>
2021-05-16 15:30:57 +0300

    Updated transactor method

--- src/main/scala/com/evolution/bootcamp/game_bot/dao/DbTransactor.scala
+++ src/main/scala/com/evolution/bootcamp/game_bot/dao/DbTransactor.scala
@@ -1,18 +1,24 @@
 package com.evolution.bootcamp.game_bot.dao
 
-import cats.effect.{Blocker, Resource, Sync}
+import cats.effect.{Async, Blocker, ContextShift, Resource}
+import com.evolution.bootcamp.game_bot.dao.config.DbConfig
 import doobie.Transactor
 import doobie.hikari.HikariTransactor
 import doobie.util.ExecutionContexts
 
 object DbTransactor {
-//  def transactor[F[_] : Sync]: Resource[F, Transactor[F]] = {
-//    for {
-//      ce <- ExecutionContexts.fixedThreadPool(32)
-//      be <- Blocker[F]
-//      xa <- HikariTransactor.newHikariTransactor[F](
-//        driverClassName =
-//      )
-//    }
-//  }
+  def transactor[F[_] : Async : ContextShift](dbConfig: DbConfig): Resource[F, Transactor[F]] = {
+    for {
+      ce <- ExecutionContexts.fixedThreadPool(32)
+      be <- Blocker[F]
+      xa <- HikariTransactor.newHikariTransactor[F](
+        driverClassName = dbConfig.dbDriverName,
+        url = dbConfig.dbUrl,
+        user = dbConfig.dbUser,
+        pass = dbConfig.dbPwd,
+        connectEC = ce,
+        blocker = be
+      )
+    } yield xa
+  }
 }
